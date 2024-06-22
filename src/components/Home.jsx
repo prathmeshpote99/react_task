501b676000af2947852ddece8b38058ee612faf4
prathmeshpote99 <poteprathmesh@gmail.com>
2024-06-22 19:20:47 +0530

    Feat: Add Navbar and Home Page with complete routing

---
+++ src/components/Home.jsx
@@ -0,0 +1,158 @@
+import React, { useRef } from "react";
+
+const Home = () => {
+  const fileInputRef = useRef(null);
+
+  const handleBoxClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileChange = (event) => {
+    const files = event.target.files;
+    console.log(files);
+  };
+
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-4 mb-3">
+          <div className="card shadow-sm" style={{ borderRadius: "10px" }}>
+            <div className="card-body">
+              <h5 className="card-title mb-4">Recent Studies</h5>
+              <div className="border rounded p-3 mb-3">
+                <div className="d-flex justify-content-between align-items-center mb-2">
+                  <div className="text-primary">
+                    <b>"Dog Bite Complaint"</b>
+                  </div>
+                  <div className="text-secondary heading2">2 days ago</div>
+                </div>
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="completed">
+                    <span className="text-success me-2">
+                      <i className="bi bi-check-circle"></i>
+                    </span>
+                    <span>Completed</span>
+                  </div>
+                </div>
+              </div>
+              <div className="border rounded p-3 mb-3">
+                <div className="d-flex justify-content-between align-items-center mb-2">
+                  <div className="text-primary">
+                    <b>"Peoples on the beach"</b>
+                  </div>
+                  <div className="text-secondary heading2">2 days ago</div>
+                </div>
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="completed">
+                    <span className="text-success me-2">
+                      <i className="bi bi-check-circle"></i>
+                    </span>
+                    <span>Completed</span>
+                  </div>
+                </div>
+              </div>
+              <div className="border rounded p-3 mb-3">
+                <div className="d-flex justify-content-between align-items-center mb-2">
+                  <div className="text-primary">"Quantum Coffee Break"</div>
+                  <div className="text-secondary heading2">2 days ago</div>
+                </div>
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="completed">
+                    <span className="text-success me-2">
+                      <i className="bi bi-check-circle"></i>
+                    </span>
+                    <span>Completed</span>
+                  </div>
+                </div>
+              </div>
+              <div className="border rounded p-3 mb-3">
+                <div className="d-flex justify-content-between align-items-center mb-2">
+                  <div className="text-primary">"Neptune's Secret Recipe"</div>
+                  <div className="text-secondary heading2">2 days ago</div>
+                </div>
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="failed">
+                    <span className="text-danger me-2">
+                      <i class="bi bi-alarm"></i>
+                    </span>
+                    <span>In progress</span>{" "}
+                  </div>
+                </div>
+              </div>
+              <div className="border rounded p-3">
+                <div className="d-flex justify-content-between align-items-center mb-2">
+                  <div className="text-primary">"Supernova Symphony"</div>
+                  <div className="text-secondary heading2">2 days ago</div>
+                </div>
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="failed">
+                    <span className="text-danger me-2">
+                      <i className="bi bi-x-circle"></i>
+                    </span>
+                    <span>Failed</span>{" "}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-8">
+          <div className="card mb-3" style={{ borderRadius: "10px" }}>
+            <div className="card-body">
+              <h5 className="card-title">Right Section</h5>
+            </div>
+            <div className="card-body">
+              <div className="d-flex align-items-center mb-4">
+                <i className="bi bi-search me-2"></i>
+                <h5 className="card-title mb-0">
+                  Get complete info about any case from AI-driven research
+                  assistance
+                </h5>
+              </div>
+              <textarea
+                className="form-control mb-3"
+                placeholder="Ex: What is React.js"
+                rows="3"
+              ></textarea>
+              <div className="upload-box mb-3" onClick={handleBoxClick}>
+                <i className="bi bi-upload"></i>
+                <p>
+                  Drag and drop here or{" "}
+                  <span className="link" onClick={handleBoxClick}>
+                    Upload a file
+                  </span>
+                </p>
+                <p>Maximum uploads: 1</p>
+                <p>File format: JPG, PNG, Docx, etc.</p>
+              </div>
+              <input
+                type="file"
+                ref={fileInputRef}
+                style={{ display: "none" }}
+                onChange={handleFileChange}
+              />
+              <h6>
+                Uploaded files: <span className="badge bg-secondary">3/5</span>
+              </h6>
+              <div className="alert alert-light d-flex align-items-center mb-2">
+                <i className="bi bi-file-earmark-pdf me-2"></i>
+                <span>Case Document.pdf</span>
+              </div>
+              <div className="alert alert-light d-flex align-items-center mb-2">
+                <i className="bi bi-file-earmark-word me-2"></i>
+                <span>Case Document.docx</span>
+              </div>
+              <div className="alert alert-light d-flex align-items-center mb-2">
+                <i className="bi bi-file-earmark-word me-2"></i>
+                <span>Case Document.docx</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-body-tertiary fixed-top">
+      <div className="container-fluid">
+        <NavLink className="navbar-brand" to="/" activeClassName="">
+          Navbar
+        </NavLink>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          className="collapse navbar-collapse justify-content-center"
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                activeClassName="active"
+                exact
+                to="/news"
+              >
+                <i className="bi bi-newspaper me-2"></i>
+                News Feed
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                activeClassName="active"
+                to="/find-lawyers"
+              >
+                <i className="bi bi-people me-2"></i>
+                Find Lawyers
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                activeClassName="active"
+                exact
+                to="/"
+              >
+                <i className="bi bi-house-door me-2"></i>
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                activeClassName="active"
+                to="/connections"
+              >
+                <i className="bi bi-bezier2 me-2"></i>
+                Connections
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" activeClassName="active" to="/chat">
+                <i className="bi bi-chat-left-text me-2"></i>
+                Chat
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+
+        <div className="d-flex align-items-center ms-auto">
+          <div className="d-none d-lg-flex">
+            <Link className="nav-link me-3" to="/settings">
+              <i className="bi bi-gear"></i>
+            </Link>
+            <Link className="nav-link me-3" to="/notifications">
+              <i className="bi bi-bell"></i>
+            </Link>
+            <Link className="nav-link me-3" to="/profile">
+              <i className="bi bi-person-circle"></i>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
